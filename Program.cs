69897bb7c3e113d0e5f0edbdee7b381f5a20824b
lenok-ar <leno4ka279a@gmail.com>
2025-03-30 00:23:40 +0700

    Writing classes

--- Program.cs
+++ Program.cs
@@ -2,53 +2,54 @@
 
 namespace task3
 {
-  public class SquareMatrix
-  {
-    int[,] matrix = new int[5, 5];
-
-    public void EnteringAndExportMatrix()
+    public class SquareMatrix
     {
-      var rand = new Random();
+        int[,] matrix = new int[5, 5];
 
-      for (int row = 0; row < matrix.GetLength(0); ++row)
-      {
-        for (int col = 0; col < matrix.GetLength(1); ++col)
+        public void EnteringAndExportMatrix()
         {
-          matrix[row, col] = rand.Next(50);
-        }
-      }
+            var rand = new Random();
 
-      for (int row = 0; row < matrix.GetLength(0); ++row)
-      {
-        for (int col = 0; col < matrix.GetLength(1); ++col)
+            for (int row = 0; row < matrix.GetLength(0); ++row)
+            {
+                for (int col = 0; col < matrix.GetLength(1); ++col)
+                {
+                    matrix[row, col] = rand.Next(50);
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); ++row)
+            {
+                for (int col = 0; col < matrix.GetLength(1); ++col)
+                {
+                    Console.Write(matrix[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static SquareMatrix operator +(SquareMatrix left, SquareMatrix right)
         {
-          Console.Write(matrix[row,col] +"\t");
+            SquareMatrix result = new SquareMatrix();
+            for (int row = 0; row < left.matrix.GetLength(0); ++row)
+            {
+                for (int col = 0; col < left.matrix.GetLength(1); ++col)
+                {
+                    result.matrix[row, col] = left.matrix[row, col] + right.matrix[row, col];
+                }
+            }
+            return result;
         }
-        Console.WriteLine();
-      }
-    }
-
-  public static SquareMatrix operator + (SquareMatrix left, SquareMatrix right)
-  { 
-    SquareMatrix result = new SquareMatrix();
-    for (int row = 0; row < left.matrix.GetLength(0); ++row)
-    {
-      for (int col = 0; col < left.matrix.GetLength(1); ++col)
-      {
-        result.matrix[row, col] = left.matrix[row, col] + right.matrix[row, col];
-      }
-    }
-    return result;
-  }
 
 
-  internal class  Program
-  {
-    static void Main(string[] args)
-    {
-      SquareMatrix matrix = new SquareMatrix();
-            
-      matrix.EnteringAndExportMatrix();
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                SquareMatrix matrix = new SquareMatrix();
+
+                matrix.EnteringAndExportMatrix();
+            }
+        }
     }
-  }
 }
