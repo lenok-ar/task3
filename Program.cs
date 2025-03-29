d9e19665c4106868f254e9e486b528d0b977a182
lenok-ar <leno4ka279a@gmail.com>
2025-03-30 00:19:35 +0700

    Writing classes

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace task3
       {
         for (int cols = 0; cols < matrix.GetLength(1); cols++)
         {
-            matrix[rows, cols] = rand.Next(50);
+          matrix[rows, cols] = rand.Next(50);
         }
       }
 
@@ -22,20 +22,33 @@ namespace task3
       {
         for (int cols = 0; cols < matrix.GetLength(1); cols++)
         {
-            Console.Write(matrix[rows,cols] +"\t");
+          Console.Write(matrix[rows,cols] +"\t");
         }
         Console.WriteLine();
       }
     }
+
+  public static SquareMatrix operator + (SquareMatrix left, SquareMatrix right)
+  { 
+    SquareMatrix result = new SquareMatrix();
+    for (int row = 0; row < left.matrix.GetLength(0); row++)
+    {
+      for (int col = 0; col < left.matrix.GetLength(1); col++)
+      {
+        result.matrix[row, col] = left.matrix[row, col] + right.matrix[row, col];
+      }
+    }
+    return result;
   }
 
-    internal class  Program
-    {
-        static void Main(string[] args)
-        {
-            SquareMatrix matrix = new SquareMatrix();   
 
-            matrix.EnteringAndExportMatrix();
-        }
+  internal class  Program
+  {
+    static void Main(string[] args)
+    {
+      SquareMatrix matrix = new SquareMatrix();
+            
+      matrix.EnteringAndExportMatrix();
     }
+  }
 }
